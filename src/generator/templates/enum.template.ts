d205475e69503fed478af55a9b238e866b276ac5
Refik ÖNAL <refikonal@gmail.com>
2021-07-08 00:58:34 +0300

    Update enum template to generate plain object and type instead of ts enum

--- src/generator/templates/enum.template.ts
+++ src/generator/templates/enum.template.ts
@@ -5,20 +5,50 @@ interface CreateEnumTemplateOptions {
   classPrefix: string;
 }
 
-export function createEnumTemplate({
+export function createEnumTemplate(options: CreateEnumTemplateOptions) {
+
+  return createObjectAndType(options);
+}
+
+function createObjectAndType({
   enumModel,
   classPrefix,
 }: CreateEnumTemplateOptions) {
+
+  const objectName = `${classPrefix}${enumModel.name}`;
+
+  const enumValues = enumModel.values.map(value => {
+    const dbName = value.dbName ?? value.name;
+    return `${value.name} : '${dbName}',`;
+  });
+
+
+  return `
+export const ${objectName} = {
+  ${enumValues.join('\n')}
+}
+
+export type ${objectName} = typeof ${objectName}[keyof typeof ${objectName}];
+
+`;
+
+}
+
+function createEnum({
+  enumModel,
+  classPrefix,
+}: CreateEnumTemplateOptions) {
+
   let template = "";
 
   template += `export enum ${classPrefix}${enumModel.name} {\n`;
 
   for (const enumValue of enumModel.values) {
-    template += `${enumValue.name}${
-      enumValue.dbName ? `= '${enumValue.dbName}'` : ""
-    },\n`;
+    template += `${enumValue.name}${enumValue.dbName ? `= '${enumValue.dbName}'` : ""
+      },\n`;
   }
   template += "}";
 
   return template;
+
 }
